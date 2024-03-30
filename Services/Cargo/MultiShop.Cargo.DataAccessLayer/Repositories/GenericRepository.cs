be9d08740dec70c4b3fe53197d53e51ce1589d3e
Alpay Akçer <info@alpayakcer.com>
2024-03-30 19:25:22 +0300

    Cargo Generic Repositorylerin tanımlaması

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+
+namespace MultiShop.Cargo.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly CargoContext _context;
+
+        public GenericRepository(CargoContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAllList()
+        {
+            var value = _context.Set<T>().ToList();
+            return value;
+        }
+
+        public T GetById(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            return value;
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
