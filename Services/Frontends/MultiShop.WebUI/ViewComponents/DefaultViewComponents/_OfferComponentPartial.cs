3dd65af177da599e0bcece02cc1185e17ae640ce
Alpay Akçer <info@alpayakcer.com>
2024-03-31 22:47:02 +0300

    Web UI Tasarımı ve View Componentlerin tasarımının eklenmesi

---
+++ Services/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_OfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _OfferComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
