d0a24e860da19d32dc02419ba8ae12e355fbdc47
Alpay Akçer <info@alpayakcer.com>
2024-04-17 23:43:53 +0300

    Payment Kredi Kart Dizayn View Page

---
+++ Services/Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Home = "Ana Sayfa";
+            ViewBag.Home1 = "Siparişler";
+            ViewBag.Home2 = "Ödeme İşlemi";
+            return View();
+        }
+    }
+}
